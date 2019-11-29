e63d2c3720cad88a698def06b8bb709016e79979
Alexander Karrer <alex.kay.rs@gmail.com>
2019-11-29 13:47:41 +0100

    added menu scenes and start of the level template

---
+++ Trampoline Ball/Assets/Scripts/LevelSelectSceneHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectSceneHandler : MonoBehaviour
+{
+    public string backButtonSceneName;
+    public string levelOneSceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void selectBackToMenuButton()
+    {
+        Debug.Log("Clicked Back to Menu");
+        SceneManager.LoadScene(backButtonSceneName);
+    }
+
+    public void selectLevelOne()
+    {
+        Debug.Log("Clicked Level 1");
+        SceneManager.LoadScene(levelOneSceneName);
+    }
+}
---
+++ Trampoline Ball/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    public string levelSceneName;
+    public string themeSceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectLevelButton()
+    {
+        Debug.Log("Clicked Level");
+        SceneManager.LoadScene(levelSceneName);
+    }
+    
+    public void SelectThemeButton()
+    {
+        Debug.Log("Clicked Theme");
+        SceneManager.LoadScene(themeSceneName);
+    }
+}
