87ffc2482f72517217bdcc48526fd1064ddd48de
Alexander Karrer <alex.kay.rs@gmail.com>
2019-12-04 17:12:30 +0100

    added more level implementation

---
+++ Trampoline Ball/Assets/Scripts/Level/CollisionHandler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Level
+{
+    public class CollisionHandler : MonoBehaviour
+    {
+    
+        public BoxCollider2D finishFlagCollider2D;
+        public EdgeCollider2D leftEdgeCollider2D;
+        public EdgeCollider2D rightEdgeCollider2D;
+        public EdgeCollider2D bottomEdgeCollider2D;
+        public EdgeCollider2D topEdgeCollider2D;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other == finishFlagCollider2D)
+            {
+                Debug.Log("hit the finish line!");
+            }
+            else if (other == leftEdgeCollider2D || other == rightEdgeCollider2D || other == bottomEdgeCollider2D ||
+                     other == topEdgeCollider2D)
+            {
+                Debug.Log("hit an outside wall");
+            }
+        }
+    }
+}
---
+++ Trampoline Ball/Assets/Scripts/Level/LevelHandler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Level
+{
+    public class LevelHandler : MonoBehaviour
+    {
+        public GameObject ball;
+        public Button pauseButton;
+        public Button playButton;
+        public Button resetButton;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            ball.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+
+
+    
+        public void SelectResetButton()
+        {
+            Debug.Log("Clicked Reset Button.");
+            ball.transform.position = new Vector3(-10,4,0);
+        }
+    
+        public void SelectPlayButton()
+        {
+            playButton.gameObject.SetActive(false);
+            pauseButton.gameObject.SetActive(true);
+            ball.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            Debug.Log("Clicked Play.");
+        }
+    
+        public void SelectPauseButton()
+        {
+            playButton.gameObject.SetActive(true);
+            pauseButton.gameObject.SetActive(false);
+            ball.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            Debug.Log("Clicked Pause.");
+        }
+    }
+}
